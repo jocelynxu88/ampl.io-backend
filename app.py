7dea9f5af6b33595e733d1406817b625d70b6c23
RaymondR-Wu <raymond.w.78.in@gmail.com>
2021-05-02 12:48:51 -0400

    yay

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -30,6 +30,69 @@ def hello_world():
 @app.route('/users')
 def users():
     return 'Hello'
+
+@app.route('/userMessages', methods=['GET'])
+def userMessages():
+    username = request.args.get('username');
+    group_ref = db.collection(u'UserMessages').document(username);
+    results = group_ref.get();
+    groupMessages = results.to_dict();
+
+    messages = [];
+
+    for i in groupMessages['Groups']:
+        message = db.collection(u'GroupChats').document(i['ChatId']).get().to_dict();
+        message['read'] = i['read'];
+        message['name'] = i['name'];
+        message['id'] = i['ChatId'];
+
+        messages.append(message);
+
+    return {'messages': messages };
+
+@app.route('/sendMessage', methods=['POST'])
+def sendMessage():
+    requestForm = request.get_json();
+
+    message_ref = db.collection(u'GroupChats').document(requestForm['ChatId']);
+    message_ref.update({u'Messages': firestore.ArrayUnion([{'message': requestForm['message'], 'sender': requestForm['username'], 'id': requestForm['id']}])});
+    
+    users = message_ref.get().to_dict();
+    users = users['members']; #now we need to go set all these to unread for the users
+
+    for user in users:
+        if user == requestForm['username']:
+            continue;
+
+        user_ref = db.collection(u'UserMessages').document(user)
+        user_groups = user_ref.get().to_dict();
+        user_groups = user_groups['Groups'];
+
+        for i in range(len(user_groups)):
+            if user_groups[i]['ChatId'] == requestForm['ChatId']:
+                user_groups[i]['read'] = False;
+                break;
+
+        user_ref.update({u'Groups': user_groups});
+    
+    return {"message": "sent"}
+
+@app.route('/getMessages', methods=['GET'])
+def getMessages():
+    messages = db.collection(u'GroupChats').document(request.args.get('ChatId')).get().to_dict();
+    user_ref = db.collection(u'UserMessages').document(request.args.get('username'));
+
+    user_groups = user_ref.get().to_dict();
+    user_groups = user_groups['Groups'];
+
+    for i in range(len(user_groups)):
+        if user_groups[i]['ChatId'] == request.args.get('ChatId'):
+            user_groups[i]['read'] = True;
+            break;
+
+    user_ref.update({u'Groups': user_groups});
+
+    return {"messages": messages['Messages']}
 
 @app.route('/goals')
 def goals():
